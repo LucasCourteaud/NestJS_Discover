c66cb782f8b711747568479cc8b7da536e8b94aa
LuckyBoy <lucas.courteaud@epitech.eu>
2022-06-27 02:35:14 +0400

    feat(nestjs) can signup and signin

--- dist/auth/auth.service.d.ts
+++ dist/auth/auth.service.d.ts
@@ -1,4 +1,17 @@
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 export declare class AuthService {
-    signup(): string;
-    signin(): string;
+    private prisma;
+    private jwt;
+    private config;
+    constructor(prisma: PrismaService, jwt: JwtService, config: ConfigService);
+    signup(dto: AuthDto): Promise<void>;
+    signin(dto: AuthDto): Promise<{
+        access_token: string;
+    }>;
+    signToken(userId: number, email: string): Promise<{
+        access_token: string;
+    }>;
 }
